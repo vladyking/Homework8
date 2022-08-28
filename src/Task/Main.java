fe26be4c3fb8ef6599af7477770bbeb9582f5629
vladyking <gvladykin@gmail.com>
2022-08-29 00:55:28 +0300

    Home work 8 Done ver. 2

--- src/Task/Main.java
+++ src/Task/Main.java
@@ -25,7 +25,6 @@ public class Main {
         System.out.println("Ответ на задание №2:");
         //int clientOS = 1; //0 - iOS, 1- Android
         //int clientDeviceYear = 2018; //clientDeviceYear >= 2015 - new smartfone
-
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке\n");
         } else if (clientOS == 0 && clientDeviceYear < 2015) {
